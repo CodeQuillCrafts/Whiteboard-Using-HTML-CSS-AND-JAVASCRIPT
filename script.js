aaa26f4b0ce3b2d5364fce196092e9d2cd917a35
Shivam Gupta <codequillcrafts@gmail.com>
2023-12-12 13:52:42 +0530

    adding files

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 const root = document.querySelector('#root');
-
 document.addEventListener('DOMContentLoaded', makeBoxes);
-
 function makeBoxes() {
     for (let i = 0; i < 10000; i++) {
         let box = createBox();
         root.appendChild(box);
         let drawing = false;
-
         box.addEventListener('mousemove', function (event) {
             if (drawing) {
                 event.target.style.backgroundColor = "white";
             }
         });
-
         document.addEventListener('click', function () {
             drawing = !drawing;
         });
     }
 }
-
 function createBox() {
     let box = document.createElement('div');
     box.classList.add('box');
